3581edd89e53c84782556ff930fb441274af00bc
ramirezalejo <ramirezalejo@live.com>
2022-05-20 00:16:27 -0400

    Update projects to match fulfill checks

--- ARS.ContractTemplating.Domain/Models/File.cs
+++ ARS.ContractTemplating.Domain/Models/File.cs
@@ -2,21 +2,51 @@ using System.Text.Json.Serialization;
 
 namespace ARS.ContractTemplating.Domain.Models;
 
+/// <summary>
+/// Entity for the Uploaded Files
+/// </summary>
 public class File
 {
-    public string Base64Data { get; set; }
+    /// <summary>
+    /// Actual file data
+    /// </summary>
+    public string? Base64Data { get; set; }
+    /// <summary>
+    /// Content type
+    /// </summary>
     public string? ContentType { get; set; }
-    public string FileName { get; set; }
-    public string Owner { get; set; }
+    /// <summary>
+    /// File Name
+    /// </summary>
+    public string? FileName { get; set; }
+    /// <summary>
+    /// User that uploaded the file
+    /// </summary>
+    public string? Owner { get; set; }
+    /// <summary>
+    /// File state, for compression checks (compress or not)
+    /// </summary>
     public byte State { get; set; }
 
+    /// <summary>
+    /// Buffer used while processing the file
+    /// </summary>
     [JsonIgnore]
     public byte[]? Buffer { get; set; }
+    /// <summary>
+    /// Original file size
+    /// </summary>
     [JsonIgnore]
     public int OriginalSize { get; set; }
+    /// <summary>
+    /// File size when compressed
+    /// </summary>
     [JsonIgnore]
     public int CompressedSize { get; set; }
-
+/// <summary>
+/// Validation for type of content to check if is a valid file
+/// </summary>
+/// <returns>bool</returns>
     public bool IsOkToCompress()
     {
         if (ContentType != null)
