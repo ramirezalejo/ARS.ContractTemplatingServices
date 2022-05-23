f6a0526d9b9a3bec5ffb7fa8080d2f4822482ab0
ramirezalejo <ramirezalejo@live.com>
2022-05-23 00:50:43 -0400

    Remove warnings

--- ARS.ContractTemplating.Services.Tests/ContractsServiceTests.cs
+++ ARS.ContractTemplating.Services.Tests/ContractsServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using ARS.ContractTemplating.Domain.Contracts.Infrastructure;
 using ARS.ContractTemplating.Domain.Models.Messages;
 using AutoFixture;
@@ -29,9 +30,12 @@ public class ContractsServiceTests
     public async Task GenerateContracts_Succeeded()
     {
         //Arrange
+        Debug.Assert(_cognitiveServicesClient != null, nameof(_cognitiveServicesClient) + " != null");
+        Debug.Assert(_logger != null, nameof(_logger) + " != null");
         var contractsService = new ContractsService(_cognitiveServicesClient, _logger);
         _cognitiveServicesClient.ReadFileUrl(Arg.Any<string>()).Returns(Task.FromResult(new List<string>()));
         //Act
+        Debug.Assert(_requestMessage != null, nameof(_requestMessage) + " != null");
         await contractsService.GenerateContract(_requestMessage);
 
         //Assert
